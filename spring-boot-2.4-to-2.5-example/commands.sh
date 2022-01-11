47a5cf8b865a5bbf772030ab659ad0598850780a
Fabian Krüger <fkrueger@vmware.com>
2022-01-11 20:25:04 +0100

    moved project files into root of repo

--- spring-boot-2.4-to-2.5-example/commands.sh
+++
@@ -1,5 +0,0 @@
-docker-compose -f docker/docker-compose.yaml down  --remove-orphans
-docker-compose -f docker/docker-compose.yaml up -d
-mvn clean package spring-boot:run
-docker exec -it docker_mysql_1 mysql -u db_user --password=db_pw
-docker exec -it docker_mysql_1 mysqldump -u db_user -p -d springboot24
